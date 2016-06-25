225525e55aac8574fe92224bc35d9d349e1f67b5
yinchaoonline <ychaowhu@foxmail.com>
2016-06-25 23:39:06 +0800

    use ejs template data

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ app.set("view engine", "ejs");
 
 //routes
 app.get("/", function (req, res) {
-
-    //send view using res.render,will look up in views folder
-    res.render("pages/index");
+    var drinks = [
+        {
+            name: 'Bloody Mary',
+            drunkness: 3
+        },
+        {
+            name: 'Martini',
+            drunkness: 5
+        },
+        {
+            name: 'Scotch',
+            drunkness: 10
+        }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else."
+        //send view using res.render,will look up in views folder
+    res.render("pages/index", {
+        drinks: drinks,
+        tagline: tagline
+    });
 });
 
 app.get("/about", function (req, res) {
